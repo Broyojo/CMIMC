04859aae0270c1d08159b15b56eeba08e0c54349
Daniel Long <zhozholong@gmail.com>
2022-03-19 14:35:09 -0400

    added opponent move prediction

--- non-fungible-greed.py
+++ non-fungible-greed.py
@@ -1,4 +1,5 @@
 import json
+from sys import stderr
 
 # INPUT FORMAT (per turn):
 
@@ -14,32 +15,49 @@ import json
 #     you would like to purchase, which must be non-negative integers
 #     summing to at most 100.
 
-def buy(buys):
-    my_history.append(buys)
-    print(json.dumps({"buys": buys}))
-
-my_history = []
-day = 0
-
-while True:
-    # read data
-    _data = json.loads(input())
-    yesterday = _data["yesterday"]
-    scores = _data["scores"]
-    my_index = _data["my_index"]
+class Strategy():
+    def make_purchases(self, yesterday, scores, self_index):
+        pass
     
-    # the actual strategy
-    buys = [0 for _ in range(10)]
-    if day == 0:
-        # an arbitrarily selected first day strategy
-        buys = [10 for _ in range(10)]
-    else:
-        # the actual strategy
+    def agrees_with(self, history, scores, self_index):
+        # this determines how well this strategy represents an opponent's actions thus far
 
+        # 1. aggregate my moves
+        moveset = []
+        for day in range(len(history)):
+            moveset.append(history[day][self_index])
+        
+        # 2. run a simulation of this strategy's moves
+        predicted_moveset = []
+        for day in range(len(history)):
+            yesterday = []
+            if day != 0:
+                yesterday = history[day-1]
+                predicted_move = self.make_purchases(yesterday, scores, self_index)
+            else:
+                predicted_move = [10 for _ in range(10)]
+            predicted_moveset.append(predicted_move)
+
+        # 3. compute the mean squared error between the simulated moves and the opponent's actual moves
+        mse = 0
+        for day in range(len(history)):
+            predicted_move = predicted_moveset[day]
+            move = moveset[day]
+            
+            for coin_i in range(len(move)):
+                dif = move[coin_i] - predicted_move[coin_i]
+                mse += dif**2
+        mse /= 10 * len(history)
+    
+        return mse
+
+class GreedyOneAhead(Strategy):
+    def make_purchases(self, yesterday, scores, self_index):
+        buys = [0 for _ in range(10)]
         # 1. aggregate a list of how much each coin was purchased
         purchases = [0 for _ in range(10)]
         for i, move in enumerate(yesterday):
-            if i != my_index:
+            if i != self_index:
                 for coin in range(len(move)):
                     purchases[coin] += move[coin]
         
@@ -49,6 +67,56 @@ while True:
             to_buy = expected_benefit.index(max(expected_benefit))
             buys[to_buy] += 1
             purchases[to_buy] += 1
+        
+        return buys
+
+class NoChange(Strategy):
+    def make_purchases(self, yesterday, scores, self_index):
+        buys = yesterday[self_index]
+        return buys
+
+strategies = [GreedyOneAhead(), NoChange()]
+def buy(buys):
+    print(json.dumps({"buys": buys}))
+
+def classify_opponents(history, scores):
+    classifications = []
+    for opponent_i in range(len(history[0])):
+        strategy_scores = []
+        for strategy in strategies:
+            strategy_scores.append(strategy.agrees_with(history, scores, opponent_i))
+        best = strategy_scores.index(min(strategy_scores))
+        classifications.append(strategies[best])
+    return classifications
+
+history = []
+day = 0
+
+while True:
+    # read data
+    _data = json.loads(input())
+    yesterday = _data["yesterday"]
+    scores = _data["scores"]
+    my_index = _data["my_index"]
     
-    buy(buys)
-    day += 1
+    if day == 0:
+        # an arbitrary first day strategy
+        buys = [10 for _ in range(10)]
+        buy(buys)
+        day += 1
+    else:
+        # attempt to classify the opponent's behavior
+        history.append(yesterday)
+        opponent_strategies = classify_opponents(history, scores)
+        
+        # simulate opponent's moves
+        today_prediction = yesterday
+        for opponent_i in range(len(today_prediction)):
+            opponent_strat = opponent_strategies[opponent_i]
+            today_prediction[opponent_i] = opponent_strat.make_purchases(yesterday, scores, opponent_i)
+        
+        # buy coins based on opponent's moves
+        buys = GreedyOneAhead().make_purchases(today_prediction, scores, my_index)
+        
+        buy(buys)
+        day += 1
