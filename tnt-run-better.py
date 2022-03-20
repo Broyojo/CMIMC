855a1a06825156ab64ee94aa7732232ebb43c157
Broyojo <dha@xoba.com>
2022-03-19 22:08:34 -0400

    hopefully should work

--- tnt-run-better.py
+++ tnt-run-better.py
@@ -49,7 +49,10 @@ def count_valid_neighbors(i, j):
     return count
 
 
-def total_expected_reward(i, j, layers, memory={}):
+memory = {}
+
+
+def total_expected_reward(i, j, layers):
     if layers == 0:
         return 0
     if (i, j) in memory:
@@ -67,45 +70,56 @@ def total_expected_reward(i, j, layers, memory={}):
 
 
 def total_safe_layers(i, j, layers, initial_layers):
-    if layers == 0:
-        return 0
     layer = initial_layers - layers
+    if layers == 0:
+        return layer
 
     for di in range(-move_dist, move_dist+1):
         for dj in range(-move_dist, move_dist+1):
             new_i = i + di
             new_j = j + dj
             if(count_valid_neighbors(new_i, new_j) > 0):
-                if total_safe_layers(new_i, new_j, layers - 1, initial_layers)
+                return total_safe_layers(new_i, new_j, layers - 1, initial_layers)
+
+    return layer
 
 
 def get_move(num_layers):
+    memory = {}
     moves = []
     for di in range(-move_dist, move_dist+1):
         for dj in range(-move_dist, move_dist+1):
             new_i = my_i + di
             new_j = my_j + dj
             if is_valid_tile(new_i, new_j):
-                moves.append((new_i, new_j))
+                moves.append(
+                    (new_i, new_j, total_expected_reward(new_i, new_j, layers=1)))
 
-    if grace_moves_left > 0:
-        new_i = my_i - 2
-        new_j = my_j + 2
-        if (new_i, new_j) in moves:
-            return (new_i, new_j)
+    # if grace_moves_left > 0:
+    #     new_i = my_i - 2
+    #     new_j = my_j + 2
+    #     if (new_i, new_j) in moves:
+    #         return (new_i, new_j)
 
     if len(moves) == 0:
         return (my_i, my_j)
 
-    better_moves = []
+    best_move = (0, 0, -1000)
     for move in moves:
-        if count_valid_neighbors(move[0], move[1]) > 0:
-            better_moves.append(move)
+        if move[2] > best_move[2]:
+            best_move = move
 
-    if len(better_moves) != 0:
-        return random.choice(better_moves)
-    else:
-        return random.choice(moves)
+    return move
+
+    # better_moves = []
+    # for move in moves:
+    #     if count_valid_neighbors(move[0], move[1]) > 0:
+    #         better_moves.append(move)
+
+    # if len(better_moves) != 0:
+    #     return random.choice(better_moves)
+    # else:
+    #     return random.choice(moves)
 
 
 def output(i, j):
